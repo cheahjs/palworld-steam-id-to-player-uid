ae5f55d616eb13cffc039a32f73ae2c45549eafb
Jun Siang Cheah <me@jscheah.me>
2024-03-24 21:53:53 +0000

    add links to other projects

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,34 @@ import PalToSteam from './components/PalToSteam.vue'
 
 <template>
   <header>
+    <nav>
+      <a href="https://palservers.xyz">Community Server List</a>
+      <a href="https://ping.palservers.xyz">Server Ping Tool</a>
+    </nav>
     <h1>Palworld Steam&lt;&gt;Player UID Converter</h1>
   </header>
   <hr />
   <main>
     <div id="app">
-      <p>
-        This only supports Steam-based servers, either co-op or dedicated.
-      </p>
+      <p>This only supports Steam-based servers, either co-op or dedicated.</p>
       <SteamToPal />
       <hr />
       <PalToSteam />
     </div>
   </main>
-  <hr/>
+  <hr />
   <footer>
-    <p>Created by <a href="https://github.com/cheahjs">@cheahjs</a><br/>
-    <a href="https://github.com/cheahjs/palworld-steam-id-to-player-uid">Source Code</a></p>
+    <p>
+      Created by <a href="https://github.com/cheahjs">@cheahjs</a><br />
+      <a href="https://github.com/cheahjs/palworld-steam-id-to-player-uid">Source Code</a>
+    </p>
   </footer>
 </template>
 
 <style scoped>
+header {
+  text-align: center;
+}
 footer {
   text-align: center;
   font-size: 0.85em;
